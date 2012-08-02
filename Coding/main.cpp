15b6930757c51d3911be52e8ae89fdf05dbc42ac
M-Adoo <liuxiaodongxiao@hotmail.com>
2012-08-02 20:55:35 +0800

    delete useless files

---
+++ Coding/main.cpp
@@ -0,0 +1,23 @@
+#include "fibonacci_heap.hpp"
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+	FibonacciHeap<int> fib;
+	FibonacciHeap<int> fib2;
+	FibonacciHeap<int>::node *p = NULL, *p2 = NULL;
+	for(int i = 1; i!= 10;  ++i)
+	{
+		fib.insert(i);
+		p=fib2.insert(10*i);
+	}
+	
+	fib.merge(fib2);
+	fib.extract_min();
+	p2 = p->right;
+	fib.decrease_key(p,0);
+	fib.decrease_key(p2,1);
+	std::cout<<"the size of list<fibonacciHeap>  is :"<<sizeof(list<FibonacciHeap<int> >)<<endl;
+	cout<<"the size of list<int> is:"<<sizeof(int)<<endl;
+	cin.get();
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-Algorithm
-=========
-
-A algorithm library. It's a collection of my learning and word.
